681e81d05e26e158e6db74ceae2e2cc48774f710
bwissal <baazizwissal13@gmail.com>
2023-09-18 15:17:48 +0100

    add chzllenge 1

---
+++ challenge1/challenge.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+	int arr[10],i,k;
+	for( i = 0;i<10;i++){
+		arr[i]=i + 1;
+	}
+	for( k =0;k<=9;k++){
+		if (arr[k] == 10)
+            printf("%d",arr[k]);
+        else
+            printf("%d, ",arr[k]);
+	}
+
+
+	return 0;
+}
+
+
